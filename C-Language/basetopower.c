f4bad75b46d40f87b3a9a1fe44057545db67fff5
Kiran,Garag <kiran.garag@cerner.com>
2020-09-15 18:58:50 +0530

    c-language

---
+++ C-Language/basetopower.c
@@ -0,0 +1,19 @@
+// cerner_2^5_2020
+
+#include <stdio.h>
+
+int main()
+{
+    int base_value,power_value;;
+    int i=1;
+    long result=1;
+
+    printf("Enter base and power value respectively");
+    scanf("%d%d",&base_value,&power_value);
+    
+    for(i=1;i<=power_value;i++)
+     result = result*base_value;
+     
+    printf("result = %d",result);
+    return 0;
+}
